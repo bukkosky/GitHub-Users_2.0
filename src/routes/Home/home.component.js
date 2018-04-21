1225c89baf43ebe55c6a3bd0783b009f67004746
marcin_ch <bukkosky@gmail.com>
2018-04-21 12:26:00 +0200

    input to add users

--- src/routes/Home/home.component.js
+++ src/routes/Home/home.component.js
@@ -1,15 +1,44 @@
 import React, { Component } from 'react';
-import { Container } from "./home.style";
+import { Container, UserLoginBox, UserButton, UserInput } from "./home.style";
+import { Wrapper } from "../../utils/styles/global.styles";
 import Header from "../../components/header/header.components";
 import Footer from "../../components/footer/footer.components";
 
 
+
 class Home extends Component {
+    state = {
+        users: [],
+        userValue: ''
+    };
+
+    addNewUser = () => {
+        console.log(this.state.userValue);
+        this.setState({
+            users: [...this.state.users, this.state.userValue],
+            userValue: ''
+        })
+    };
+
+    userOnChange = (event) => {
+        this.setState({userValue: event.target.value});
+    };
+
     render() {
         return (
             <Container>
                 <Header />
-                home
+
+                <Wrapper>
+                    <UserLoginBox>
+                        <div>Please add GitHub user name to observable</div>
+                        <UserInput value={this.state.userValue} onChange={this.userOnChange}/>
+                        <UserButton onClick={this.addNewUser}>
+                            Add User
+                        </UserButton>
+                    </UserLoginBox>
+                </Wrapper>
+
                 <Footer />
             </Container>
         );
