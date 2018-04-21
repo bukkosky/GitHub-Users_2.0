7ed97f4b959dd598cc44f6b25be35a5a5cbbd304
marcin_ch <bukkosky@gmail.com>
2018-04-21 09:23:10 +0200

    add styled components

--- src/routes/Home/home.component.js
+++ src/routes/Home/home.component.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import { Container } from "./home.style";
 
 
 class Home extends Component {
     render() {
         return (
-            <div>
+            <Container>
                 home
-            </div>
+            </Container>
         );
     }
 }
